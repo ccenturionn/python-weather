dcb6aa823063097816bef5a03cda52f31216c2be
ccenturionnMC <ccenturionnMC@gmail.com>
2019-11-09 12:16:41 -0800

    Beginning of forecast functionality

--- python-weather.py
+++ python-weather.py
@@ -1,4 +1,7 @@
+#!/usr/bin/env python3
+
 import requests, json, os
+from datetime import date
 
 api_key = "79fb1e0e0654b11e1319112ffe57fba4"
 
@@ -12,6 +15,8 @@ response = requests.get(complete_url)
 
 x = response.json()
 
+
+
 if x["cod"] != "404":
 	y = x["main"]
 	current_temp = int(y["temp"] - 273.15)
@@ -23,9 +28,11 @@ if x["cod"] != "404":
 	weather_description = z[0]["description"]
 
 	os.system('cls' if os.name == 'nt' else 'clear')
-	print("Temp:", current_temp, "degrees C")
-	print("Pressure:", current_pressure)
-	print("Humidity:", current_humidity, "percent")
-	print("Description:", weather_description)
+	print("The temperature is currently", current_temp, "degrees Celsius")
+	print("The air pressure is currently", current_pressure)
+	print("The humidity is currently", current_humidity, "percent")
+	print("The weather is currently", weather_description)
+	print(json.dumps(x, indent=2, sort_keys=True))
+	print(date.today())
 else:
 	print("City not found.")
